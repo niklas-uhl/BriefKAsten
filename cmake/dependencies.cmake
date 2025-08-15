07223a80302c71464820bc336fc75c4532acaa19
Niklas Uhl <uhl@kit.edu>
2025-08-15 14:49:05 +0200

    Get rid of old examples.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -13,17 +13,3 @@ FetchContent_Declare(
   GIT_TAG f0873f8
   SYSTEM
 )
-
-FetchContent_Declare(
-  range-v3
-  URL https://github.com/ericniebler/range-v3/archive/0.12.0.zip
-  SYSTEM
-  SOURCE_SUBDIR NON_EXISTANT
-)
-
-FetchContent_Declare(
-  CLI11
-  GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
-  GIT_TAG v2.5.0
-  SYSTEM
-)
