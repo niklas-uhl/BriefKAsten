c70fbbf5ac6f481fe0a93431d6168c9c7f6ac1c9
Niklas Uhl <uhl@kit.edu>
2025-08-15 07:34:55 +0200

    Merger and splitter which encode envelope metdata (required for indirection).

--- src/message-queue/aggregators.hpp
+++ src/message-queue/aggregators.hpp
@@ -20,8 +20,10 @@
 #pragma once
 
 #include <iterator>
+#include <ranges>
 #include <span>
 #include "./detail/concepts.hpp"
+#include "./detail/view_adaptors.hpp"
 
 namespace message_queue::aggregation {
 
@@ -95,6 +97,52 @@ struct SentinelSplitter {
 };
 
 static_assert(Splitter<SentinelSplitter<int>, int, std::vector<int>>);
+
+template <typename EnvType, typename BufferContainer>
+concept SerializableEnvelope =
+    MPIBuffer<BufferContainer> && Envelope<EnvType> &&
+    std::is_convertible_v<typename EnvType::message_value_type, std::ranges::range_value_t<BufferContainer>> &&
+    std::is_convertible_v<PEID, std::ranges::range_value_t<BufferContainer>> &&
+    std::is_convertible_v<std::size_t, std::ranges::range_value_t<BufferContainer>>;
+
+struct EnvelopeSerializationMerger {
+    template <MPIBuffer BufferContainer,
+              SerializableEnvelope<BufferContainer> EnvType>  // requires that both the message value type and
+    void operator()(BufferContainer& buffer, PEID /* buffer_destination */, PEID /* my_rank */, EnvType envelope) {
+        using env_type = EnvType::message_value_type;
+        using buffer_type = std::ranges::range_value_t<BufferContainer>;
+        std::vector<buffer_type> combined(envelope.message.size() + 2);  // extra space for receiver and size
+        combined[0] = static_cast<buffer_type>(envelope.receiver);
+        combined[1] = static_cast<buffer_type>(envelope.message.size());
+        // Copy the message into the combined vector, starting from index 2
+        // and transform the message values to the buffer type.
+        auto converted_message = envelope.message | std::views::transform([](env_type const& value) -> buffer_type {
+                                     return static_cast<buffer_type>(value);
+                                 });
+        std::ranges::copy(converted_message, combined.begin() + 2);
+        buffer.insert(buffer.end(), combined.begin(), combined.end());
+    }
+    template <MPIBuffer BufferContainer, SerializableEnvelope<BufferContainer> EnvType>
+    size_t estimate_new_buffer_size(BufferContainer const& buffer,
+                                    PEID /* buffer_destination */,
+                                    PEID /* my_rank */,
+                                    EnvType const& envelope) const {
+        return buffer.size() + envelope.message.size() + 2;  // 2 for receiver and size
+    }
+};
+
+template <typename MessageType>
+struct EnvelopeSerializationSplitter {
+    template <MPIBuffer BufferContainer>
+    auto operator()(BufferContainer const& buffer, PEID /* buffer_origin */, PEID /* my_rank */) const {
+        return buffer | chunk_by_embedded_size(1) | std::views::transform([](auto chunk) {
+                   PEID receiver = static_cast<PEID>(chunk[0]);
+                   auto message = chunk | std::views::drop(2) |
+                                  std::views::transform([](auto& elem) { return static_cast<MessageType>(elem); });
+                   return MessageEnvelope{std::move(message), 0, receiver, 0};
+               });
+    }
+};
 
 struct NoOpCleaner {
     template <typename BufferContainer>
