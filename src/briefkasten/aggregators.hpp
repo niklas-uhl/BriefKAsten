09d0f82bcf94b7217a69ac904d8bed26667e0b0a
Niklas Uhl <uhl@kit.edu>
2025-10-15 18:55:36 +0200

    Working version.

--- src/briefkasten/aggregators.hpp
+++ src/briefkasten/aggregators.hpp
@@ -19,9 +19,14 @@
 
 #pragma once
 
+#include <concepts>
 #include <iterator>
+#include <kamping/mpi_datatype.hpp>
 #include <ranges>
 #include <span>
+#include <tuple>
+#include <type_traits>
+#include <print>
 #include "./detail/concepts.hpp"
 #include "./detail/view_adaptors.hpp"
 
@@ -102,10 +107,23 @@ struct SentinelSplitter {
 
 static_assert(Splitter<SentinelSplitter<int>, int, std::vector<int>>);
 
+template <typename From, typename To>
+struct all_members_convertible_to : std::false_type {};
+
+template <typename First, typename Second, typename To>
+struct all_members_convertible_to<std::pair<First, Second>, To>
+    : std::conjunction<std::is_convertible<First, To>, std::is_convertible<Second, To>> {};
+
+template <typename... Ts, typename To>
+struct all_members_convertible_to<std::tuple<Ts...>, To> : std::conjunction<std::is_convertible<Ts, To>...> {};
+
+template <typename From, typename To>
+concept is_serializable_to = std::is_convertible_v<From, To> || all_members_convertible_to<From, To>::value;
+
 template <typename EnvType, typename BufferContainer>
 concept SerializableEnvelope =
     MPIBuffer<BufferContainer> && Envelope<EnvType> &&
-    std::is_convertible_v<typename EnvType::message_value_type, std::ranges::range_value_t<BufferContainer>> &&
+    is_serializable_to<typename EnvType::message_value_type, std::ranges::range_value_t<BufferContainer>> &&
     std::is_convertible_v<PEID, std::ranges::range_value_t<BufferContainer>> &&
     std::is_convertible_v<std::size_t, std::ranges::range_value_t<BufferContainer>>;
 
@@ -115,15 +133,41 @@ struct EnvelopeSerializationMerger {
     void operator()(BufferContainer& buffer, PEID /* buffer_destination */, PEID /* my_rank */, EnvType envelope) {
         using env_type = EnvType::message_value_type;
         using buffer_type = std::ranges::range_value_t<BufferContainer>;
-        std::vector<buffer_type> combined(envelope.message.size() + 2);  // extra space for receiver and size
+
+        constexpr std::size_t elem_size = [&] {
+            if constexpr (requires { std::tuple_size<env_type>::value; }) {
+                return std::tuple_size<env_type>::value;
+            } else {
+                return 1;
+            }
+        }();
+        std::vector<buffer_type> combined((envelope.message.size() * elem_size) + 2);  // extra space for receiver and size
         combined[0] = static_cast<buffer_type>(envelope.receiver);
-        combined[1] = static_cast<buffer_type>(envelope.message.size());
+        combined[1] = static_cast<buffer_type>(envelope.message.size() * elem_size);
         // Copy the message into the combined vector, starting from index 2
         // and transform the message values to the buffer type.
-        auto converted_message = envelope.message | std::views::transform([](env_type const& value) -> buffer_type {
-                                     return static_cast<buffer_type>(value);
-                                 });
-        std::ranges::copy(converted_message, combined.begin() + 2);
+        auto converted_message =
+            envelope.message | std::views::transform([](env_type const& value) /* -> buffer_type */ {
+                if constexpr (elem_size > 1) {
+                    std::array<buffer_type, elem_size> buf;
+                    std::size_t i = 0;
+                    auto fn = [&](auto const& val, auto /* idx */) {
+                        buf[i++] = static_cast<buffer_type>(val);
+                    };
+		    kamping::internal::for_each_tuple_field(value, fn);
+                    // std::apply(fn, value);
+                    return buf;
+                } else {
+                    return static_cast<buffer_type>(value);
+                }
+            });
+	// std::println("converted_message {}", converted_message);
+        if constexpr (elem_size > 1) {
+            std::ranges::copy(converted_message | std::views::join, combined.begin() + 2);
+        } else {
+            std::ranges::copy(converted_message, combined.begin() + 2);
+        }
+	/* std::println("msg:{}, combined: {}", envelope.message, combined); */
         buffer.insert(buffer.end(), combined.begin(), combined.end());
     }
     template <MPIBuffer BufferContainer, SerializableEnvelope<BufferContainer> EnvType>
@@ -135,16 +179,43 @@ struct EnvelopeSerializationMerger {
     }
 };
 
+static_assert(Merger<EnvelopeSerializationMerger, int, std::vector<int>>);
+static_assert(Merger<EnvelopeSerializationMerger, std::pair<int, int>, std::vector<int>>);
+
 template <typename MessageType>
 struct EnvelopeSerializationSplitter {
     template <MPIBuffer BufferContainer>
     auto operator()(BufferContainer const& buffer, PEID /* buffer_origin */, PEID /* my_rank */) const {
-        return buffer | chunk_by_embedded_size(1) | std::views::transform([](auto chunk) {
-                   PEID receiver = static_cast<PEID>(chunk[0]);
-                   auto message = chunk | std::views::drop(2) |
-                                  std::views::transform([](auto& elem) { return static_cast<MessageType>(elem); });
-                   return MessageEnvelope{std::move(message), 0, receiver, 0};
-               });
+      constexpr std::size_t elem_size = [&] {
+	if constexpr (requires { std::tuple_size<MessageType>::value; }) {
+	  return std::tuple_size<MessageType>::value;
+	} else {
+	  return 1;
+	}
+      }();
+      return buffer | chunk_by_embedded_size(1) | std::views::transform([&](auto chunk) {
+	PEID receiver = static_cast<PEID>(chunk[0]);
+	auto message_data = chunk | std::views::drop(2);  /* | std::views::transform([](auto& /\* elem *\/) { */
+	/*   /\* return MessageType{}; *\/ */
+	/*   return static_cast<MessageType>(elem); */
+        /* }); */
+	if constexpr (elem_size > 1) {
+	  auto message = message_data | std::views::chunk(elem_size) | std::views::transform([&](auto tup) {
+	      std::array<typename std::tuple_element<0, MessageType>::type, elem_size> arr{};
+	      std::size_t i = 0;
+	      for (auto const& val : tup) {
+		arr[i++] = val;
+	      }
+	      return std::apply([](auto const&... vals) { return MessageType{vals...}; }, arr);
+	    });
+	  return MessageEnvelope{std::move(message), 0, receiver, 0};
+	} else {
+	  auto message = message_data | std::views::transform([](auto const& elem) {
+	      return static_cast<MessageType>(elem);
+	    });
+          return MessageEnvelope{std::move(message), 0, receiver, 0};
+	  }
+      });
     }
 };
 
